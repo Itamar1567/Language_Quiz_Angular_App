4712bbdf215ce6d16ed93fe2471ad76decd296d4
Itamar1567 <Itamarbaharav@gmail.com>
2025-09-05 15:34:50 -0400

    Frontend changes commit

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10d57659b6d365903068f70e88c2f9216779a93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617ac68c5584a0eeba5f71fb1afae8492a041bdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
