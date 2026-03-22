70b9c745ffb60adc38445c798339c0cc0cecfecc
Itamar1567 <Itamarbaharav@gmail.com>
2026-03-22 15:04:06 -0400

    Add new url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-
     production: "true",
     CLERK_PUBLISHABLE_KEY: "pk_test_cmVsYXRlZC1oZWRnZWhvZy00My5jbGVyay5hY2NvdW50cy5kZXYk",
-    BACKEND_URL: "https//:lang-quiz-be-ecc6dbawgxf4gea0.canadacentral-01.azurewebsites.net/"
+    BACKEND_URL: "https://languagequizappbackend-hthfhmejazcgcfb3.canadacentral-01.azurewebsites.net/"
 
 };
